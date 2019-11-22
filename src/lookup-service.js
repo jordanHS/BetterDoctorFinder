83b17f42d8ab5307bed68c74d7dc58ed60d94d90
Student <example@example.com>
2019-11-22 10:51:08 -0800

    add API key & url

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -1,6 +1,6 @@
 export class WeatherService {
     async getWeatherByCity(city) {
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=47.6062%2C122.3321&user_location=47.6062%2C122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`);
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
         } catch(error) {
