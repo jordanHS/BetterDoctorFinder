5f9f8eedcc37ebb9067e0e4b2f8dc258ed662347
Jordan Safford <jordansafford@gmail.com>
2019-12-20 11:10:49 +0100

    add error & empty array messages

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -1,7 +1,7 @@
 export class LookupService {
     async findDoctors(firstName,lastName) {
         try{
-        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=wa-seattle&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`);
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
         } catch(error) {
--- src/main.js
+++ src/main.js
@@ -8,22 +8,31 @@ $(document).ready(function() {
   $('#findDoctors').click(function() {
     const firstName = $('#firstName').val();
     const lastName = $("#lastName").val();
-    const query = $("query").val();   
         $('#firstName').val("");
         $("#lastName").val("");
-        $("query").val("");
-
     (async () => {
       let lookupService = new LookupService();
-      const response = await lookupService.findDoctors(firstName, lastName, query);
+      const response = await lookupService.findDoctors(firstName, lastName);
       getElements(response);
       
     })();
 
   const getElements = function(response) {
-      $('.showDoctors').text(`${response.data[0].practices[0].name} - Address: ${response.data[0].practices[0].visit_address.street}
-      Phone: ${response.data[0].practices[0].phones[0].number}`);
-    console.log(response.data[0].practices[0]);
+    if(response.meta.error){
+      if(response.meta.http_status_code == 400)
+          $('.showDoctors').text(`Please enter a name`);
+      else
+          $('.showDoctors').text(`Error 401: Unauthorized Request`);
+      
+      return;
     }
+    if (response.data) {
+      if(response.data.length)
+        $('.showDoctors').text(`${response.data[0].practices[0].name} - Address: ${response.data[0].practices[0].visit_address.street}
+      Phone: ${response.data[0].practices[0].phones[0].number}`);
+      else
+        $('.showDoctors').text(`No doctors matching that name were found.`);
+    }
+  }
   });
 });
